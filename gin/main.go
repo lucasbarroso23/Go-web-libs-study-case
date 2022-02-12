a1184fa39520bbe513df39e9be640a45102a74cc
Lucas Barroso <55262061+lucasbarroso23@users.noreply.github.com>
2022-02-12 08:37:31 -0300

    folder structure and createUser route

--- gin/main.go
+++ gin/main.go
@@ -1,15 +1,20 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"gin/configs"
+	"gin/routes"
+
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
 	router := gin.Default()
 
-	router.GET("/", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"data": "Hello from Gin & mongoDB",
-		})
-	})
+	// run database
+	configs.ConnectDB()
+
+	// routes
+	routes.UserRoute(router)
 
 	router.Run("localhost:6000")
 }
