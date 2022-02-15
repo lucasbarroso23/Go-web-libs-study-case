baffea510ce1e6b18bf615af47b7c81fd4a5518d
Lucas Barroso <55262061+lucasbarroso23@users.noreply.github.com>
2022-02-14 23:04:55 -0300

    edit/delete/getAll endpoints

--- echo/controllers/user_controller.go
+++ echo/controllers/user_controller.go
@@ -87,3 +87,131 @@ func GetUser(c echo.Context) error {
 		Data:    &echo.Map{"data": user},
 	})
 }
+
+func EditUser(c echo.Context) error {
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	userId := c.Param("userId")
+	var user models.User
+	defer cancel()
+
+	objId, _ := primitive.ObjectIDFromHex(userId)
+
+	// validate the request body
+	if err := c.Bind(&user); err != nil {
+		return c.JSON(http.StatusBadRequest, responses.UserResponse{
+			Status:  http.StatusBadRequest,
+			Message: "error",
+			Data:    &echo.Map{"data": err.Error()},
+		})
+	}
+
+	// use the validator library to validate required fields
+	if validationErr := validate.Struct(&user); validationErr != nil {
+		return c.JSON(http.StatusBadRequest, responses.UserResponse{
+			Status:  http.StatusBadRequest,
+			Message: "error",
+			Data:    &echo.Map{"data": validationErr.Error()},
+		})
+	}
+
+	update := bson.M{
+		"name":     user.Name,
+		"location": user.Location,
+		"title":    user.Title,
+	}
+
+	result, err := userCollection.UpdateOne(ctx, bson.M{"id": objId}, bson.M{"$set": update})
+	if err != nil {
+		return c.JSON(http.StatusInternalServerError, responses.UserResponse{
+			Status:  http.StatusInternalServerError,
+			Message: "error",
+			Data:    &echo.Map{"data": err.Error()},
+		})
+	}
+
+	// get updated user details
+	var userUpdated models.User
+	if result.MatchedCount == 1 {
+		err := userCollection.FindOne(ctx, bson.M{"id": objId}).Decode(&userUpdated)
+
+		if err != nil {
+			return c.JSON(http.StatusInternalServerError, responses.UserResponse{
+				Status:  http.StatusInternalServerError,
+				Message: "error",
+				Data:    &echo.Map{"data": err.Error()},
+			})
+		}
+	}
+
+	return c.JSON(http.StatusOK, responses.UserResponse{
+		Status:  http.StatusOK,
+		Message: "success",
+		Data:    &echo.Map{"data": userUpdated},
+	})
+}
+
+func DeteleUser(c echo.Context) error {
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	userId := c.Param("userId")
+	defer cancel()
+
+	objId, _ := primitive.ObjectIDFromHex(userId)
+
+	result, err := userCollection.DeleteOne(ctx, bson.M{"id": objId})
+	if err != nil {
+		return c.JSON(http.StatusInternalServerError, responses.UserResponse{
+			Status:  http.StatusInternalServerError,
+			Message: "error",
+			Data:    &echo.Map{"data": err.Error()},
+		})
+	}
+
+	if result.DeletedCount < 1 {
+		c.JSON(http.StatusNotFound, responses.UserResponse{
+			Status:  http.StatusNotFound,
+			Message: "error",
+			Data:    &echo.Map{"data": "User with specified ID not found"},
+		})
+	}
+
+	return c.JSON(http.StatusOK, responses.UserResponse{
+		Status:  http.StatusOK,
+		Message: "success",
+		Data:    &echo.Map{"data": "User successfully deleted"},
+	})
+}
+
+func GetAllUsers(c echo.Context) error {
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	var users []models.User
+	defer cancel()
+
+	results, err := userCollection.Find(ctx, bson.M{})
+	if err != nil {
+		return c.JSON(http.StatusInternalServerError, responses.UserResponse{
+			Status:  http.StatusInternalServerError,
+			Message: "error",
+			Data:    &echo.Map{"data": err.Error()},
+		})
+	}
+
+	//reading from the db in an optimal way
+	defer results.Close(ctx)
+	for results.Next(ctx) {
+		var singleUser models.User
+		if err := results.Decode(&singleUser); err != nil {
+			c.JSON(http.StatusInternalServerError, responses.UserResponse{
+				Status:  http.StatusInternalServerError,
+				Message: "error",
+				Data:    &echo.Map{"data": err.Error()},
+			})
+		}
+		users = append(users, singleUser)
+	}
+
+	return c.JSON(http.StatusOK, responses.UserResponse{
+		Status:  http.StatusOK,
+		Message: "success",
+		Data:    &echo.Map{"data": users},
+	})
+}
--- echo/routes/user_routes.go
+++ echo/routes/user_routes.go
@@ -9,4 +9,7 @@ import (
 func UserRoute(e *echo.Echo) {
 	e.POST("/user", controllers.CreateUser)
 	e.GET("/user/:userId", controllers.GetUser)
+	e.PUT("/user/:userId", controllers.EditUser)
+	e.DELETE("/user/:userId", controllers.DeteleUser)
+	e.GET("/users", controllers.GetAllUsers)
 }
