5a0c27a76e8a4f8079cf31417e8888b8b3cd93d5
Lucas Barroso <55262061+lucasbarroso23@users.noreply.github.com>
2022-02-15 22:29:50 -0300

    fiber setup and createUser/getUser endpoints

---
+++ fiber/routes/user_routes.go
@@ -0,0 +1,12 @@
+package routes
+
+import (
+	"fiber/controllers"
+
+	"github.com/gofiber/fiber/v2"
+)
+
+func UserRoute(app *fiber.App) {
+	app.Post("/user", controllers.CreateUser)
+	app.Get("/user/:userId", controllers.GetUser)
+}
