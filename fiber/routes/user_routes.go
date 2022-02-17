49ba547decfc5b80e48adb50ba1c10af0fe185db
Lucas Barroso <55262061+lucasbarroso23@users.noreply.github.com>
2022-02-17 20:53:18 -0300

    getAllUsers endpoint

--- fiber/routes/user_routes.go
+++ fiber/routes/user_routes.go
@@ -9,4 +9,7 @@ import (
 func UserRoute(app *fiber.App) {
 	app.Post("/user", controllers.CreateUser)
 	app.Get("/user/:userId", controllers.GetUser)
+	app.Put("/user/:userId", controllers.EditUser)
+	app.Delete("/user/:userId", controllers.DeleteUser)
+	app.Get("/users", controllers.GetAllUsers)
 }
